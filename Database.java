9ed425ec22af6d2dd79d490339f76e67ab8aaaa6
Jesper Nielsen <jesp74m8@stud.kea.dk>
2022-09-12 10:29:30 +0200

    Made Main, Database and Superhero.java with basic functions

---
+++ Database.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Database {
+    private List<Superhero> superheroes;
+
+    public Database() {
+        superheroes = new List<Superheroes>;
+    }
+
+    public void addSuperhero(Superhero superhero) {
+        superheroes.add(superhero);
+    }
+
+    public void addSuperheroes(List<Superhero> superheroes) {
+        for (int i = 0; i < superheroes.size(); i++) {
+            this.superheroes.add(superheroes.get(i));
+        }
+    }
+
+    public void deleteSuperhero(Superhero superhero) {
+    }
+
+    public void deleteSuperheroes() {
+
+    }
+}
---
+++ Superhero.java
@@ -0,0 +1,32 @@
+public class Superhero {
+    private int age;
+    private double height;
+    private double weight;
+
+    public Superhero() {
+
+    }
+
+    public getAge() {
+
+    }
+
+    public setAge() {
+
+    }
+
+    public getHeight() {
+
+    }
+
+    public setHeight() {
+
+    }
+
+    public getWeight() {
+
+    }
+    public setWeight() {
+
+    }
+}
